fff72e4cae50101020c6864704283849861d20f0
airscanner100 <154707579+airscanner100@users.noreply.github.com>
2025-01-07 20:57:06 -0700

    Upload

---
+++ rtlsdr_collect_sig_group_avg_v1.py
@@ -0,0 +1,212 @@
+from pylab import *
+from rtlsdr import *
+from sys import platform
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from datetime import datetime
+
+# Initialize the sdr object
+sdr = RtlSdr()
+
+# Set a flag for Data Collection Mode (1) or Test Mode (0)
+data_flag = 1
+
+# Set a Plot Flag
+plot_flag = 1
+
+# Set a flag to subtract off baseline trace
+sub_flag = 0
+
+# Initiate Variables
+num_samples = 8 * 256 * 1024  # Number of samples to collect  8*256*1024
+direction = 270               # Compass direction 0=North, 90=East, 180=South, 270=West
+incline_ew = 90               # Angle of inclination of dish from earth horizon ref east = 0
+incline_ns = 135              # Angle of inclination of dish from earth horizon ref north = 0
+psd_nfft = 8192 * 2           # Length of PSD vectors (freq and magnitude)  8192
+
+# Set Variables for Data Collection (1) or Test Mode (0)
+if data_flag == 1:   
+    num_group_loop = 100	    # Set Number of Loops  100
+    num_loops = 165             # Set Number of Loops  165 for 24 hrs
+    pause_group_time = 0.5  	# Pause Time (sec)     1
+    pause_loop_time = 360	    # Pause Time (sec)     300
+    low_limit = 0.10			# Low limit for plot   0.10 (v4)
+elif data_flag == 0:
+    num_group_loop = 5		    # Set Number of Loops
+    num_loops = 2		        # Set Number of Loops
+    pause_group_time = 1	    # Pause Time (sec)
+    pause_loop_time = 1		    # Pause Time (sec)
+    low_limit = 0.00		    # Low limit for plot
+    
+# Configure Device
+sdr.sample_rate = 2.4e6         # 2.4e6 
+sdr.center_freq = 1420.4e6
+sdr.gain = 'auto' # 'auto' 
+
+print("SDR Gain = " + str(sdr.gain))
+
+# Prepare a Time Date String for Directory
+now = datetime.now()
+date_time_dir = now.strftime("%Y%m%d__%H%M")
+        
+# Determine the OS
+if platform == "linux" or platform == "linux2":
+    # linux
+    print("OS is Linux ...")
+    file_path_dir = "/home/airscanner100/Documents/github/upload/" + date_time_dir + "/"
+elif platform == "darwin":
+    # OS X
+    print("OS is Mac ...")
+    file_path_dir = "/home/airscanner100/Documents/github/upload" + date_time_dir + "/"
+elif platform == "win32":
+    # Windows
+    print("OS is Windows ...")
+    file_path_dir = "C:\\Scan_Files\\data\\" + date_time_dir + "\\"
+
+# Create Directory if Necessary
+if not os.path.exists(file_path_dir):
+    os.mkdir(file_path_dir)
+
+# Print the Start Time
+print("Start Time : ", time.ctime())
+
+# Collect Data
+for i in range(num_loops):
+
+    # Initiate Variables
+    count = 1    # Counter
+
+    # String Iteration
+    loop = str(i+1).rjust(3, '0')
+
+    # Initialize Arrays for Averaging
+    psd_array = np.zeros(psd_nfft)
+    freq_array = np.zeros(psd_nfft)
+
+    for j in range(num_group_loop):
+      
+        # Update the time stamp
+        now = datetime.now()
+        date_time_cur = now.strftime("%Y%m%d__%H%M%S")
+
+        # Collect Data
+        samples = sdr.read_samples(num_samples)
+
+        # Close Plot for PSD
+        plt.close()
+    
+        # Generate the PSD of the Data Collected
+        psd_samp, freq_samp = plt.psd(samples, NFFT=psd_nfft, Fs=sdr.sample_rate / 1e6, Fc=sdr.center_freq / 1e6)
+
+        # Add to the baseline PSD array
+        if count == 1:
+            psd_samp_base = psd_samp
+        
+	    # Add to the loop PSD array
+        psd_array = psd_array + psd_samp
+        freq_array = freq_array + freq_samp
+        
+		# Calculate the average of the averaged PSD Array
+        psd_samp_mean = np.average(psd_samp)
+   
+        # Print Status
+        print(date_time_cur + " Group " + str(i+1) + "/" + str(num_loops) + 
+            ", File " + str(j+1) + "/" + str(num_group_loop) + 
+            ", PSDAvg=" + f"{psd_samp_mean:.2e}" +
+            ", SDRGain=" + str(sdr.gain))
+       
+        # Close Plot for PSD
+        plt.close()
+       
+        # Update Counter
+        count = count + 1
+
+        # Pause
+        time.sleep(pause_group_time)
+
+    # Print Update
+    print("Collected Loop " + str(i+1) + " of " + str(num_loops))
+
+    # Prepare a Time Date String for Filename
+    now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    time_now = now.strftime("%H:%M:%S")
+    date_time = now.strftime("%Y%m%d__%H%M%S__")
+
+    # String Iteration
+    group_loop = str(j+1).rjust(3, '0')
+
+    # Create a Complete Filename
+    file_path = file_path_dir + loop + "__avg__" + date_time + f"{direction:03d}" + "_" + f"{incline_ns:03d}" + "_" + f"{incline_ew:03d}"
+
+    # Generate an Average PSD
+    print('Generating an Averaged Signal Result')
+    print('------------------------------------')
+    psd_array_avg = psd_array / (count - 1)
+    freq_array_avg = freq_array / (count - 1)
+
+    # Subtract off baseline
+    if sub_flag == 1:
+        psd_array_avg = psd_array_avg - psd_samp_base
+
+    # Calculate the average of the averaged PSD Array
+    psd_array_avg_mean = np.average(psd_array_avg)
+
+    # Save the File
+    np.save(file_path, [psd_array_avg,freq_array_avg],psd_array_avg_mean)
+
+    # Close any Plots that Might be Open
+    plt.close()
+
+    # Plot Data
+    if plot_flag == 1:
+
+        # Turn on Interactive
+        plt.ion()
+
+        # Plot the PSD of the Averaged Data
+        plt.figure(1)
+        plt.plot(freq_array_avg, psd_array_avg)
+        plt.xlabel('Frequency (MHz)')
+        plt.ylabel('Samp Relative power (dB)')
+        plt.ylim(low_limit,None)
+        plt.title(str(date_time) + "," + str(count-1) + 
+            " Traces, Loop " + str(i+1) + ", AVg=" + 
+            f"{psd_array_avg_mean:.2e}" + ", SDRGain=" + str(sdr.gain))
+
+        # Save the Plot
+        plt.savefig(file_path)
+
+        # Show the Plot
+        plt.show()
+
+        # Pause
+        time.sleep(4)
+        
+        # Close the Average Plot
+        plt.close()
+            
+    # Pause Before the Next Loop
+    time.sleep(pause_loop_time)
+
+    # Close the File
+    del file_path
+    
+    # Close the Plot
+    plt.close()
+
+# Close the SDR
+sdr.close()
+
+# Close all Plots
+plt.close()
+
+# End Program
+# Print the End Time
+print("End Time : ", time.ctime())
+print('End Program')
